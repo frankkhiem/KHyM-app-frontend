e115fbf46e08783115b8ebddb20503ae62e36b2b
frankkhiem <khiem.ngg@gmail.com>
2021-09-15 16:13:47 +0700

    Create modal login/register

--- js/index.js
+++ js/index.js
@@ -10,17 +10,60 @@ var closedSearchBar = $('.search-bar__closed');
 searchTool.addEventListener('click', function() {
 	overlaySearchBar.style.display = 'block';
 	searchBar.classList.add('show');
-})
+});
 
 overlaySearchBar.addEventListener('click', function(e) {
 	searchBar.classList.remove('show');
-	this.style.display = 'none';
-})
+	this.style.display = null;
+});
 
 closedSearchBar.addEventListener('click', function(e) {
 	searchBar.classList.remove('show');
-	overlaySearchBar.style.display = 'none';
-})
+	overlaySearchBar.style.display = null;
+});
 
 
 // click vào btn user hiện modal đăng nhập/đăng ký
+var userTool = $('#show-modal-login');
+var modalLoginContainer = $('.modal-container.overlay');
+var modalLogin = $('.modal-login');
+var closedModalLogin = $('.modal-login__closed');
+var closedModalLoginFn = function() {
+	modalLoginContainer.classList.remove('show');
+};
+
+userTool.addEventListener('click', function() {
+	modalLoginContainer.classList.add('show');
+});
+
+modalLoginContainer.addEventListener('click', closedModalLoginFn);
+
+closedModalLogin.addEventListener('click', closedModalLoginFn);
+
+modalLogin.addEventListener('click', function(e) {
+	e.stopPropagation();
+});
+
+// chuyển tab giữa đăng nhập và đăng ký
+const loginTab = $('#login-tab');
+const loginForm = $('#login-form');
+const registerTab = $('#register-tab');
+const registerForm = $('#register-form');
+
+loginTab.addEventListener('click', function() {
+		let tabActived = $('.modal-login-tab.active');
+		let formShowed = $('.modal-login-form.show');
+		tabActived.classList.remove('active');
+		formShowed.classList.remove('show');
+		loginTab.classList.add('active');
+		loginForm.classList.add('show');
+});
+
+registerTab.addEventListener('click', function() {
+		let tabActived = $('.modal-login-tab.active');
+		let formShowed = $('.modal-login-form.show');
+		tabActived.classList.remove('active');
+		formShowed.classList.remove('show');
+		registerTab.classList.add('active');
+		registerForm.classList.add('show');
+});
